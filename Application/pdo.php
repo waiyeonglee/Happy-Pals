357bd8fc9eadbb6fa22652c0fa6de9ad103544d1
Philip Lee <leephiliphy@gmail.com>
2021-02-28 01:07:58 +0800

    hello

--- Application/pdo.php
+++ Application/pdo.php
@@ -3,4 +3,5 @@ $dsn = getenv('CLOUDSQL_DSN');
 $user = getenv('CLOUDSQL_USER');
 $password = getenv('CLOUDSQL_PASSWORD');
 $pdo = new PDO($dsn, $user, $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>

a04977fee508ac4ce2d8b01d2646fb9b21107036
Philip Lee <leephiliphy@gmail.com>
2021-02-27 14:49:16 +0800

    Initial commit

---
+++ add.php
@@ -0,0 +1,73 @@
+<?php
+
+session_start();
+
+// Demand a SESSION
+if ( ! isset($_SESSION['account']) ) {
+    die('ACCESS DENIED');
+}
+
+require_once "bootstrap.php"; require_once "pdo.php";
+
+if ( isset($_POST['cancel']) ) {
+    header('Location: index.php');
+    return;
+}
+
+// Check to see if we have some POST data, if we do process it
+if ( isset($_POST['year']) && isset($_POST['mileage']) && isset($_POST['make']) && isset($_POST['model']) ) {
+	if ( strlen($_POST['year']) < 1 || strlen($_POST['mileage']) < 1 || strlen($_POST['make']) < 1 || strlen($_POST['model']) < 1 ) {
+		$_SESSION['failure'] = "All fields are required";
+        header("Location: add.php");
+        return;
+	} elseif ( !is_numeric($_POST['mileage']) ) {
+		$_SESSION['failure'] = "Mileage must be an integer";
+		header('Location: add.php');
+		return;
+	} elseif ( !is_numeric($_POST['year']) ) {
+		$_SESSION['failure'] = "Year must be an integer";
+		header('Location: add.php');
+		return;
+	} else {
+		$_SESSION['success'] = "Record added";
+		$stmt = $pdo->prepare('INSERT INTO automobiles (make, model, year, mileage) VALUES ( :mk, :md, :yr, :mi)');
+		$stmt->execute(array(
+			':mk' => $_POST['make'],
+			':md' => $_POST['model'],
+			':yr' => $_POST['year'],
+			':mi' => $_POST['mileage'])
+		);
+		header('Location: index.php');
+		return;
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Philip Lee Hann Yung's Automobile Tracker</title>
+</head>
+<body>
+<div class="container">
+<h1>Tracking Autos for <?echo $_SESSION['account']?></h1>
+<?php
+if ( isset($_SESSION['failure']) ) {
+    echo('<p style="color: red;">'.htmlentities($_SESSION['failure'])."</p>\n");
+	unset($_SESSION['failure']);
+}
+?>
+<form method="post">
+<p>Make:
+<input type="text" name="make" size="60"/></p>
+<p>Model:
+<input type="text" name="model" size="60"/></p>
+<p>Year:
+<input type="text" name="year"/></p>
+<p>Mileage:
+<input type="text" name="mileage"/></p>
+<input type="submit" value="Add">
+<input type="submit" name="cancel" value="Cancel">
+</form>
+</div>
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Philip Lee Hann Yung - Automobiles Database</title>
+<?php require_once "bootstrap.php"; ?>
+<style>td, th {padding:5px}</style>
+</head>
+<body>
+<div class='container'>
+<h1>Welcome to Automobiles Database</h1>
+<?php // Do not put any HTML above this line
+
+session_start();
+
+if ( ! isset($_SESSION['account']) ) {
+    echo ("<p><a href='login.php'>Please log in</a></p><p>Attempt to <a href='add.php'>add data</a> without logging in</p>");
+}
+
+else {
+	require_once "pdo.php";
+	
+	if ( isset($_SESSION['success']) ) {
+		echo('<p style="color: green;">'.htmlentities($_SESSION['success'])."</p>\n");
+		unset($_SESSION['success']);
+	}
+	
+	if ( isset($_SESSION['error']) ) {
+		echo('<p style="color: red;">'.htmlentities($_SESSION['error'])."</p>\n");
+		unset($_SESSION['error']);
+	}
+	
+	
+	$stmt = $pdo->query("SELECT autos_id, make, model, year, mileage FROM automobiles ORDER BY make");
+	if ( $stmt->rowCount() === 0 ) {
+		echo ("<p>No rows found</p>");
+	} else {
+		echo("<table border='1'><tr><th>Make</th><th>Model</th><th>Year</th><th>Mileage</th><th>Action</th></tr>");
+		while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+			echo "<tr><td>";
+			echo(htmlentities($row['make']));
+			echo("</td><td>");
+			echo(htmlentities($row['model']));
+			echo("</td><td>");
+			echo(htmlentities($row['year']));
+			echo("</td><td>");
+			echo(htmlentities($row['mileage']));
+			echo("</td><td>");
+			echo('<a href="edit.php?autos_id='.$row['autos_id'].'">Edit</a> / ');
+			echo('<a href="delete.php?autos_id='.$row['autos_id'].'">Delete</a>');
+			echo("</td></tr>");
+		}
+		echo("</table><br/>");
+	}
+	echo ("<p><a href='add.php'>Add New Entry</a></p><p><a href='logout.php'>Logout</a></p>");
+}
+?>
+</div>
+</body>

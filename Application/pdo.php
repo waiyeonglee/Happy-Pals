5b09360a7f0825aebf4a43420b891c42dc69320d
Philip Lee <leephiliphy@gmail.com>
2021-02-28 11:25:24 +0800

    deploy

--- Application/pdo.php
+++ Application/pdo.php
@@ -2,7 +2,6 @@
 $dsn = getenv('CLOUDSQL_DSN');
 $user = getenv('CLOUDSQL_USER');
 $password = getenv('CLOUDSQL_PASSWORD');
-//$pdo = new PDO($dsn, $user, $password);
-$pdo = new PDO("mysql:host=sql12.freemysqlhosting.net; port=3306; dbname=sql12395648;unix_socket=/cloudsql/happy-pals:asia-southeast2:happy-pals-my-sql", 'sql12395648', 'dWTrM7792J');
+$pdo = new PDO($dsn, $user, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
